d3cbf687021cb8e8370dc21aa8be33f0a5ae0f60
keshav-koli <keshavkoli5588@gmail.com>
2024-07-09 14:11:55 +0530

    completed

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
-var clutter='';
-function createBubble(){
-for(let i=1;i<=170;i++){
-    var rn=Math.floor(Math.random()*10);
-    clutter+=`<div class="bubble">${rn}   
-    </div>`
+var timer = 60;
+var pbtm = document.querySelector("#pbottom");
+var HitVal = document.querySelector("#HitVal");
+var scoreVal = document.querySelector("#scoreVal");
+var hitrn;
+var score = 0;
+function createBubble() {
+  var clutter = "";
+  for (let i = 1; i <= 130; i++) {
+    var rn = Math.floor(Math.random() * 10);
+    clutter += `<div class="bubble">${rn}   
+        </div>`;
+  }
+  pbtm.innerHTML = clutter;
+}
+function runtimer() {
+  var time_counter = setInterval(() => {
+    if (timer > 0) {
+      timer--;
+      let Timer = document.querySelector("#Timer");
+      Timer.textContent = timer;
+    } else {
+      clearInterval(time_counter);
+      pbtm.innerHTML = `<h1 id='gameOver'>Game Over</h1>`;
+    }
+  }, 1000);
 }
 
-let pbtm=document.querySelector('#pbottom') ;
-pbtm.innerHTML=clutter;
+function hit() {
+  hitrn = Math.floor(Math.random() * 10);
+  HitVal.textContent = hitrn;
 }
+function Increasescore() {
+  score += 10;
+  scoreVal.textContent = score;
+}
+pbtm.addEventListener("click", (dets) => {
+  var bubbleClicked = Number(dets.target.textContent);
+  console.log(bubbleClicked);
+  if (bubbleClicked === hitrn) {
+    Increasescore();
+    hit();
+    createBubble();
+  } else {
+    if (timer > 0) {
+      createBubble();
+    }
+  }
+});
+
 createBubble();
-var timer=60;
-function runtimer(){
-var time_counter=setInterval(() => {
-    if(timer>0){
-    timer--;
-    let Timer=document.querySelector('#Timer') ;
-    Timer.textContent=timer;
-    }
-    else{
-        clearInterval(time_counter);
-    }
-}, 1000);
-}
+hit();
 runtimer();
