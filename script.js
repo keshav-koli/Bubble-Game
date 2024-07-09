cee438bebca05ff8ab7c57d7cf411de7385774ba
keshav-koli <keshavkoli5588@gmail.com>
2024-07-09 13:27:42 +0530

    create function Runtimer()

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-let bubble=document.querySelector('.bubble') 
 var clutter='';
-for(let i=1;i<=70;i++){
-    clutter+=`<div class="bubble">5   
-                </div>`
+function createBubble(){
+for(let i=1;i<=170;i++){
+    var rn=Math.floor(Math.random()*10);
+    clutter+=`<div class="bubble">${rn}   
+    </div>`
 }
+
+let pbtm=document.querySelector('#pbottom') ;
+pbtm.innerHTML=clutter;
+}
+createBubble();
+var timer=60;
+function runtimer(){
+var time_counter=setInterval(() => {
+    if(timer>0){
+    timer--;
+    let Timer=document.querySelector('#Timer') ;
+    Timer.textContent=timer;
+    }
+    else{
+        clearInterval(time_counter);
+    }
+}, 1000);
+}
+runtimer();
